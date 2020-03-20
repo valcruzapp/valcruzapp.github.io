35a27c753616fe3a23c50605668bff2f8fd9f6ab
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2020-03-20 02:19:48 -0300

    add v2

---
+++ precache-manifest.b6d01ca1813f11d519b9a4c107f0fda6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c018b4fb36bc71d09191ee0e8036a53e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a72e35698a8be34ba86",
+    "url": "/static/css/main.a62e03a2.chunk.css"
+  },
+  {
+    "revision": "ef43f36fe5af931a8fa9",
+    "url": "/static/js/2.1d5da3d2.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.1d5da3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a72e35698a8be34ba86",
+    "url": "/static/js/main.d4dcb8c3.chunk.js"
+  },
+  {
+    "revision": "1272b2a9efd1b332f905",
+    "url": "/static/js/runtime-main.c0e7a6be.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a18bdaa5e071c9cdd531482ff475177.js"
+  "/precache-manifest.b6d01ca1813f11d519b9a4c107f0fda6.js"
 );
 
 self.addEventListener('message', (event) => {
